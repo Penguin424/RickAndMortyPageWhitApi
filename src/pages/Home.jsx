e5deb18db21ee599c9b930b19e96511e95a85d34
Pablo Rizo <cosbiomedev@gmail.com>
2020-06-26 16:57:13 -0500

    Images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,12 +19,13 @@ const Home = () => {
     return (
         <div className="container">
             <h1 className="text-center">Bienvenido</h1>
+
             <div className="row">
                 <div className="col-md-6">
                     <div className="card mb-3">
                         <div className="row g-0">
                             <div className="col-md-6">
-                                <img width="100%" src="https://firebasestorage.googleapis.com/v0/b/portafolio-c8c78.appspot.com/o/1581209828325descarga.jpeg?alt=media&token=dfa8c537-1266-4607-a807-5d84e5b8e955" alt=""/>
+                                <img width="100%" src={data.length > 0 ? data[1].image : 'ddsd'} alt=""/>
                             </div>
                             <div className="col-md-6">
                                 <div className="card-body">
@@ -35,10 +36,52 @@ const Home = () => {
                     </div>
                 </div>
                 <div className="col-md-6">
-                    
-                </div>
-                
+                    <div className="card mb-3">
+                        <div className="row g-0">
+                            <div className="col-md-6">
+                                <img width="100%" src={data.length > 0 ? data[2].image : 'ddsd'} alt=""/>
+                            </div>
+                            <div className="col-md-6">
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>    
             </div>
+
+            <div className="row">
+                <div className="col-md-6">
+                    <div className="card mb-3">
+                        <div className="row g-0">
+                            <div className="col-md-6">
+                                <img width="100%" src={data.length > 0 ? data[0].image : 'ddsd'} alt=""/>
+                            </div>
+                            <div className="col-md-6">
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div className="card mb-3">
+                        <div className="row g-0">
+                            <div className="col-md-6">
+                                <img width="100%" src={data.length > 0 ? data[3].image : 'ddsd'} alt=""/>
+                            </div>
+                            <div className="col-md-6">
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>    
+            </div>
+
         </div>
     );
 };
